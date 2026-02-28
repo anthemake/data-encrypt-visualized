7c7ec4783af16877183b5568b75bd4841d8cf4df
Anthemake <hello@anthemake.com>
2026-02-28 08:32:30 -0500

    Update packages and tailwind config

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+import EncryptionDemo from "./components/EncryptionDemo";
+
+export default function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen bg-gray-100">
+      <EncryptionDemo />
+    </main>
+  );
+}
